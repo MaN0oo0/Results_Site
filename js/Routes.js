2dfdc7676683da3cf003e15a02bf2425b54ce298
MaN0oo0 <orbanm316@gmail.com>
2023-10-25 10:48:22 +0300

    Update V85

--- js/Routes.js
+++ js/Routes.js
@@ -1,10 +1,12 @@
 $(".Tops").load("../Components/thanwyTops.html");
 
 let renderBody = $("#renderBody");
-
+$(renderBody).hide();
 const routes = $(".quick");
 
 $(routes).on("click", function (e) {
+  $(".containerx").removeClass("d-none");
+  $(".containerx").fadeIn(10);
   let target = e.target.classList[1];
   console.log(target);
   if (!e.target.classList[1]) {
@@ -12,12 +14,15 @@ $(routes).on("click", function (e) {
   }
   switch (target) {
     case "thanwy":
+      $(".containerx").fadeOut(1000);
+      $(renderBody).show(1100);
       renderBody.load("../Components/thanwyComp.html", "", () => {
-        $("#Seating_No").focus();
+        $("#btnsub").focus();
       });
       break;
 
     default:
+      $(".containerx").fadeOut(1000);
       renderBody.load("../Components/coningSoon.html", "", () => {
         $("#focs").focus();
       });
