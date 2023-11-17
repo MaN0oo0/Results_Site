4a68b79d401d861f592af8905b4a6f7cc7fb9b3f
MaN0oo0 <orbanm316@gmail.com>
2023-11-17 13:36:04 +0200

    Update V111

--- js/Routes.js
+++ js/Routes.js
@@ -12,24 +12,24 @@ $(routes).on("click", function (e) {
   if (!e.target.classList[1]) {
     location.reload();
   }
-  switch (target) {
-    case "thanwy":
-      // $(".containerx").fadeOut(1000);
-      // $(renderBody).show(1100);
-      // renderBody.load("../Components/thanwyComp.html", "", () => {
-      //   $("#btnsub").focus();
-      // });
-      window.open("../Components/thanwyComp.html", "_self");
-      break;
+  // switch (target) {
+  //   case "thanwy":
+  //     // $(".containerx").fadeOut(1000);
+  //     // $(renderBody).show(1100);
+  //     // renderBody.load("../Components/thanwyComp.html", "", () => {
+  //     //   $("#btnsub").focus();
+  //     // });
+  //     window.open("../Components/thanwyComp.html", "_self");
+  //     break;
 
-    case "ebtedaey":
-    case "aedady":
-    case "deplom":
-      // $(".containerx").fadeOut(1000);
-      // renderBody.load("../Components/coningSoon.html", "", () => {
-      //   $("#focs").focus();
-      // });
-      window.open("../Components/coningSoon.html", "_self");
-      break;
-  }
+  //   case "ebtedaey":
+  //   case "aedady":
+  //   case "deplom":
+  //     // $(".containerx").fadeOut(1000);
+  //     // renderBody.load("../Components/coningSoon.html", "", () => {
+  //     //   $("#focs").focus();
+  //     // });
+  //     window.open("../Components/coningSoon.html", "_self");
+  //     break;
+  // }
 });
